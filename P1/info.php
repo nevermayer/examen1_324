68de5d280081d79077ccf574bd09c570aaba8a20
nevermayer <jrgamespro@gmail.com>
2023-10-09 11:28:31 -0400

    Add files via upload

---
+++ P1/info.php
@@ -0,0 +1,78 @@
+<?php
+include 'header.php'
+?>
+<main>
+  <div class="container marketing">
+    <!-- START THE FEATURETTES -->
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">MISIÓN.</span></h2>
+        <p class="lead">Formar profesionales idóneos con calidad humana, ética, valores, excelencia científica, compromiso social, capacidad crítica y creativa para potenciar el desarrollo de la ciencia y la tecnología en el área de la Informática en concordancia con los requerimientos de la sociedad y sus instituciones, desempeñándose con éxito en el ámbito regional, nacional e internacional.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading fw-normal lh-1">VISIÓN.</h2>
+        <p class="lead">Ser la unidad académica líder a nivel nacional y un referente de alto nivel en la formación de profesionales del área de la Informática, que aporta a la región y el país no solo con sus graduados sino también con proyectos de investigación y extensión de alto impacto relacionados con ciencia y tecnología.
+
+        </p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading fw-normal lh-1">OBJETIVOS.</h2>
+        <p class="lead">Formar profesionales altamente calificados en ciencia y tecnología del área de la Informática para responder a las necesidades de la sociedad.</p>
+        <p class="lead">Conducir proyectos de Investigación científica en el área de la Informática.</p>
+        <p class="lead">Conducir proyectos de Interacción social en la región y el país.</p>
+      </div>
+      <div class="col-md-5">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading fw-normal lh-1">VALORES.</h2>
+        <p class="lead">En la carrera se fomentan los siguientes valores: </p>
+        <ul>
+          <li>Honestidad</li>
+          <li>Ética</li>
+          <li>Responsabilidad</li>
+          <li>Equidad e igualdad</li>
+          <li>Respeto a la diversidad cultural</li>
+          <li>Respeto por el medio ambiente</li>
+          <li>Rigor profesional y científico</li>
+          <li>Puntualidad</li>
+        </ul>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+  </div>
+
+</main>
+<?php
+include 'footer.php'
+?>
---
+++ P1/instituto.php
@@ -0,0 +1,20 @@
+<?php
+include 'header.php'
+?>
+<main>
+
+  <section class="py-5 text-center container">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Instituto de Investigaciones en Informática</h1>
+        <p class="lead text-body-secondary">El Instituto de Investigaciones en Informática (III), es parte de la Carrera de Informática de la Facultad de Ciencias Puras y Naturales. Inaugurada el 16 de marzo de 1990, el III es un centro de investigación científica y de formación universitaria, de diferentes áreas de las ciencias de la computación y tecnología. Sus tópicos de investigación incluyen informática teórica, educación y TIC’s, diseño y desarrollo de sistemas, simulación y modelado de sistemas e inteligencia artificial.</p>
+        <p>
+          <a href="#" class="btn btn-primary my-2">Mas información</a>
+        </p>
+      </div>
+    </div>
+  </section>
+</main>
+<?php
+include 'footer.php'
+?>
